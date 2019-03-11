a4f9ad14748af8a21f1921dbe01e21755d4e3cda
Cedric Chou <cdric.chou@gmail.com>
2019-03-11 14:17:57 -0700

    fixed a typo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import MainFrame from './containers/MainFrame';
-import Draft from './containers/Draft';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <MainFrame /> */}
-        <Draft />
+        <MainFrame />
       </div>
     );
   }
